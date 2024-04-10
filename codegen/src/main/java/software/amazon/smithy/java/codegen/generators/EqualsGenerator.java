4d139edb7984f09348fc617d6264515ddf3475b9
Hunter Mellema <hpm@amazon.com>
2024-04-10 09:11:44 -0600

    Add equals generator

--- codegen/src/main/java/software/amazon/smithy/java/codegen/generators/EqualsGenerator.java
+++ codegen/src/main/java/software/amazon/smithy/java/codegen/generators/EqualsGenerator.java
@@ -6,12 +6,32 @@
 package software.amazon.smithy.java.codegen.generators;
 
 
+import software.amazon.smithy.java.codegen.writer.JavaWriter;
 import software.amazon.smithy.model.shapes.Shape;
 
-record EqualsGenerator(Shape shape) implements Runnable {
+final class EqualsGenerator implements Runnable {
+    private final JavaWriter writer;
+    private final Shape shape;
+
+    EqualsGenerator(JavaWriter writer, Shape shape) {
+        this.writer = writer;
+        this.shape = shape;
+    }
 
     @Override
     public void run() {
-
+        writer.write("""
+            @Override
+            public boolean equals(Object other) {
+                if (other == this) {
+                    return true;
+                }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                $1T that = ($1T) other;
+                return ${C|};
+            }
+            """, );
     }
 }
