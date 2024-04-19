4b163c8b5f8e3549a1ca486958e09b6ae6d2b2f0
Hunter Mellema <hpm@amazon.com>
2024-04-19 08:12:01 -0600

    Add generated code

---
+++ examples/restjson-example/build/smithyprojections/restjson-example/source/java-codegen/software/amazon/smithy/java/runtime/example/codegen/model/PutPersonInput.java
@@ -0,0 +1,379 @@
+/**
+ * Header Line 1
+ * Header Line 2
+ */
+
+package software.amazon.smithy.java.runtime.example.codegen.model;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import software.amazon.smithy.java.runtime.core.schema.PreludeSchemas;
+import software.amazon.smithy.java.runtime.core.schema.SdkSchema;
+import software.amazon.smithy.java.runtime.core.schema.SdkShapeBuilder;
+import software.amazon.smithy.java.runtime.core.schema.SerializableShape;
+import software.amazon.smithy.java.runtime.core.serde.ShapeDeserializer;
+import software.amazon.smithy.java.runtime.core.serde.ShapeSerializer;
+import software.amazon.smithy.java.runtime.core.serde.ToStringSerializer;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeType;
+import software.amazon.smithy.utils.SmithyBuilder;
+import software.amazon.smithy.utils.SmithyGenerated;
+import software.amazon.smithy.utils.SmithyUnstableApi;
+
+
+@SmithyGenerated
+public final class PutPersonInput implements SerializableShape {
+    public static final ShapeId ID = ShapeId.from("smithy.example#PutPersonInput");
+
+    private static final SdkSchema SCHEMA_NAME = SdkSchema.memberBuilder(0, "name", PreludeSchemas.STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_FAVORITE_COLOR = SdkSchema.memberBuilder(1, "favoriteColor", PreludeSchemas.STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_AGE = SdkSchema.memberBuilder(2, "age", PreludeSchemas.INTEGER)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_BIRTHDAY = SdkSchema.memberBuilder(3, "birthday", SharedSchemas.BIRTHDAY)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_BINARY = SdkSchema.memberBuilder(4, "binary", PreludeSchemas.BLOB)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_NOT_REQUIRED_BOOL = SdkSchema.memberBuilder(5, "notRequiredBool", PreludeSchemas.BOOLEAN)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_REQUIRED_BOOL = SdkSchema.memberBuilder(6, "requiredBool", PreludeSchemas.BOOLEAN)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_QUERY_PARAMS = SdkSchema.memberBuilder(7, "queryParams", SharedSchemas.MAP_LIST_STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_DEFAULT_BOOLEAN = SdkSchema.memberBuilder(8, "defaultBoolean", PreludeSchemas.BOOLEAN)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_DEFAULT_LIST = SdkSchema.memberBuilder(9, "defaultList", SharedSchemas.LIST_OF_STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_DEFAULT_MAP = SdkSchema.memberBuilder(10, "defaultMap", SharedSchemas.MAP_STRING_STRING)
+        .id(ID)
+        .build();
+
+    static final SdkSchema SCHEMA = SdkSchema.builder()
+        .id(ID)
+        .type(ShapeType.STRUCTURE)
+        .members(
+            SCHEMA_NAME,
+            SCHEMA_FAVORITE_COLOR,
+            SCHEMA_AGE,
+            SCHEMA_BIRTHDAY,
+            SCHEMA_BINARY,
+            SCHEMA_NOT_REQUIRED_BOOL,
+            SCHEMA_REQUIRED_BOOL,
+            SCHEMA_QUERY_PARAMS,
+            SCHEMA_DEFAULT_BOOLEAN,
+            SCHEMA_DEFAULT_LIST,
+            SCHEMA_DEFAULT_MAP
+        )
+        .build();
+
+    private final String name;
+    private final String favoriteColor;
+    private final Integer age;
+    private final Instant birthday;
+    private final byte[] binary;
+    private final Boolean notRequiredBool;
+    private final boolean requiredBool;
+    private final Map<String, List<String>> queryParams;
+    private final boolean defaultBoolean;
+    private final List<String> defaultList;
+    private final Map<String, String> defaultMap;
+
+    private PutPersonInput(Builder builder) {
+        this.name = SmithyBuilder.requiredState("name", builder.name);
+        this.favoriteColor = builder.favoriteColor;
+        this.age = builder.age;
+        this.birthday = SmithyBuilder.requiredState("birthday", builder.birthday);
+        this.binary = builder.binary;
+        this.notRequiredBool = builder.notRequiredBool;
+        this.requiredBool = SmithyBuilder.requiredState("requiredBool", builder.requiredBool);
+        this.queryParams = builder.queryParams != null ? Collections.unmodifiableMap(builder.queryParams) : null;
+        this.defaultBoolean = SmithyBuilder.requiredState("defaultBoolean", builder.defaultBoolean);
+        this.defaultList = Collections.unmodifiableList(builder.defaultList);
+        this.defaultMap = Collections.unmodifiableMap(builder.defaultMap);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String favoriteColor() {
+        return favoriteColor;
+    }
+
+    public int age() {
+        return age;
+    }
+
+    public Instant birthday() {
+        return birthday;
+    }
+
+    /**
+     * This is a binary blob! Yay!
+     * It has quite a few documentation traits added to it.
+     *
+     * @see <a href="https://www.example.com/">Homepage</a>
+     * @see <a href="https://www.example.com/api-ref">API Reference</a>
+     * @since 1.3.4.5.6
+     * @deprecated As of 1.4.5.6. This shape is no longer used.
+     */
+    @SmithyUnstableApi
+    @Deprecated(since = "1.4.5.6")
+    public byte[] binary() {
+        return binary;
+    }
+
+    public boolean notRequiredBool() {
+        return notRequiredBool;
+    }
+
+    public Boolean requiredBool() {
+        return requiredBool;
+    }
+
+    public Map<String, List<String>> queryParams() {
+        return queryParams != null ? queryParams : Collections.emptyMap();
+    }
+
+    public boolean hasQueryParams() {
+        return queryParams != null;
+    }
+
+    public Boolean defaultBoolean() {
+        return defaultBoolean;
+    }
+
+    public List<String> defaultList() {
+        return defaultList;
+    }
+
+    public boolean hasDefaultList() {
+        return defaultList != null;
+    }
+
+    public Map<String, String> defaultMap() {
+        return defaultMap != null ? defaultMap : Collections.emptyMap();
+    }
+
+    public boolean hasDefaultMap() {
+        return defaultMap != null;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringSerializer.serialize(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        PutPersonInput that = (PutPersonInput) other;
+        return Objects.equals(name, that.name)
+               && Objects.equals(favoriteColor, that.favoriteColor)
+               && Objects.equals(age, that.age)
+               && Objects.equals(birthday, that.birthday)
+               && Arrays.equals(binary, that.binary)
+               && Objects.equals(notRequiredBool, that.notRequiredBool)
+               && Objects.equals(requiredBool, that.requiredBool)
+               && Objects.equals(queryParams, that.queryParams)
+               && Objects.equals(defaultBoolean, that.defaultBoolean)
+               && Objects.equals(defaultList, that.defaultList)
+               && Objects.equals(defaultMap, that.defaultMap);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(name, favoriteColor, age, birthday, notRequiredBool, requiredBool, queryParams, defaultBoolean, defaultList, defaultMap);
+        result = 31 * result + Arrays.hashCode(binary);
+        return result;
+
+    }
+
+    @Override
+    public void serialize(ShapeSerializer serializer) {
+        // Placeholder. Do nothing
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for {@link PutPersonInput}.
+     */
+    public static final class Builder implements SdkShapeBuilder<PutPersonInput> {
+        private String name;
+        private String favoriteColor;
+        private Integer age;
+        private Instant birthday = Instant.parse("1985-04-12T23:20:50.52Z");
+        private byte[] binary;
+        private Boolean notRequiredBool;
+        private boolean requiredBool;
+        private Map<String, List<String>> queryParams;
+        private Boolean defaultBoolean = true;
+        private List<String> defaultList = new ArrayList<>();
+        private Map<String, String> defaultMap = new LinkedHashMap<>();
+
+        private Builder() {}
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder favoriteColor(String favoriteColor) {
+            this.favoriteColor = favoriteColor;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder birthday(Instant birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder binary(byte[] binary) {
+            this.binary = binary;
+            return this;
+        }
+
+        public Builder notRequiredBool(boolean notRequiredBool) {
+            this.notRequiredBool = notRequiredBool;
+            return this;
+        }
+
+        public Builder requiredBool(boolean requiredBool) {
+            this.requiredBool = requiredBool;
+            return this;
+        }
+
+        public Builder queryParams(Map<String, List<String>> queryParams) {
+            this.queryParams = queryParams != null ? new LinkedHashMap<>(queryParams) : null;
+            return this;
+        }
+
+        public Builder putAllQueryParams(Map<String, List<String>> queryParams) {
+            if (this.queryParams == null) {
+                this.queryParams = new LinkedHashMap<>(queryParams);
+            } else {
+                this.queryParams.putAll(queryParams);
+            }
+            return this;
+        }
+
+        public Builder putQueryParams(String key, List<String> value) {
+           if (this.queryParams == null) {
+               this.queryParams = new LinkedHashMap<>();
+           }
+           this.queryParams.put(key, value);
+           return this;
+        }
+
+        public Builder defaultBoolean(boolean defaultBoolean) {
+            this.defaultBoolean = defaultBoolean;
+            return this;
+        }
+
+        public Builder defaultList(Collection<String> defaultList) {
+            this.defaultList = defaultList != null ? new ArrayList<>(defaultList) : null;
+            return this;
+        }
+
+        public Builder addAllDefaultList(Collection<String> defaultList) {
+            if (this.defaultList == null) {
+                this.defaultList = new ArrayList<>(defaultList);
+            } else {
+                this.defaultList.addAll(defaultList);
+            }
+            return this;
+        }
+
+        public Builder defaultList(String defaultList) {
+            if (this.defaultList == null) {
+                this.defaultList = new ArrayList<>();
+            }
+            this.defaultList.add(defaultList);
+            return this;
+        }
+
+        public Builder defaultList(String... defaultList) {
+            if (this.defaultList == null) {
+                this.defaultList = new ArrayList<>();
+            }
+            Collections.addAll(this.defaultList, defaultList);
+            return this;
+        }
+
+        public Builder defaultMap(Map<String, String> defaultMap) {
+            this.defaultMap = defaultMap != null ? new LinkedHashMap<>(defaultMap) : null;
+            return this;
+        }
+
+        public Builder putAllDefaultMap(Map<String, String> defaultMap) {
+            if (this.defaultMap == null) {
+                this.defaultMap = new LinkedHashMap<>(defaultMap);
+            } else {
+                this.defaultMap.putAll(defaultMap);
+            }
+            return this;
+        }
+
+        public Builder putDefaultMap(String key, String value) {
+           if (this.defaultMap == null) {
+               this.defaultMap = new LinkedHashMap<>();
+           }
+           this.defaultMap.put(key, value);
+           return this;
+        }
+
+        @Override
+        public PutPersonInput build() {
+            return new PutPersonInput(this);
+        }
+
+        @Override
+        public Builder deserialize(ShapeDeserializer decoder) {
+            // PLACEHOLDER. Needs implementation
+            return this;
+        }
+    }
+}
+
---
+++ examples/restjson-example/build/smithyprojections/restjson-example/source/java-codegen/software/amazon/smithy/java/runtime/example/codegen/model/PutPersonOutput.java
@@ -0,0 +1,325 @@
+/**
+ * Header Line 1
+ * Header Line 2
+ */
+
+package software.amazon.smithy.java.runtime.example.codegen.model;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.SequencedSet;
+import software.amazon.smithy.java.runtime.core.schema.PreludeSchemas;
+import software.amazon.smithy.java.runtime.core.schema.SdkSchema;
+import software.amazon.smithy.java.runtime.core.schema.SdkShapeBuilder;
+import software.amazon.smithy.java.runtime.core.schema.SerializableShape;
+import software.amazon.smithy.java.runtime.core.serde.ShapeDeserializer;
+import software.amazon.smithy.java.runtime.core.serde.ShapeSerializer;
+import software.amazon.smithy.java.runtime.core.serde.ToStringSerializer;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeType;
+import software.amazon.smithy.utils.SmithyBuilder;
+import software.amazon.smithy.utils.SmithyGenerated;
+
+
+@SmithyGenerated
+public final class PutPersonOutput implements SerializableShape {
+    public static final ShapeId ID = ShapeId.from("smithy.example#PutPersonOutput");
+
+    private static final SdkSchema SCHEMA_NAME = SdkSchema.memberBuilder(0, "name", PreludeSchemas.STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_FAVORITE_COLOR = SdkSchema.memberBuilder(1, "favoriteColor", PreludeSchemas.STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_AGE = SdkSchema.memberBuilder(2, "age", PreludeSchemas.INTEGER)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_BIRTHDAY = SdkSchema.memberBuilder(3, "birthday", SharedSchemas.BIRTHDAY)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_STATUS = SdkSchema.memberBuilder(4, "status", PreludeSchemas.INTEGER)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_LIST = SdkSchema.memberBuilder(5, "list", SharedSchemas.LIST_OF_STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_SET = SdkSchema.memberBuilder(6, "set", SharedSchemas.SET_OF_STRING)
+        .id(ID)
+        .build();
+
+    private static final SdkSchema SCHEMA_REQUIRED_LIST = SdkSchema.memberBuilder(7, "requiredList", SharedSchemas.LIST_OF_STRING)
+        .id(ID)
+        .build();
+
+    static final SdkSchema SCHEMA = SdkSchema.builder()
+        .id(ID)
+        .type(ShapeType.STRUCTURE)
+        .members(
+            SCHEMA_NAME,
+            SCHEMA_FAVORITE_COLOR,
+            SCHEMA_AGE,
+            SCHEMA_BIRTHDAY,
+            SCHEMA_STATUS,
+            SCHEMA_LIST,
+            SCHEMA_SET,
+            SCHEMA_REQUIRED_LIST
+        )
+        .build();
+
+    private final String name;
+    private final String favoriteColor;
+    private final int age;
+    private final Instant birthday;
+    private final Integer status;
+    private final List<String> list;
+    private final SequencedSet<String> set;
+    private final List<String> requiredList;
+
+    private PutPersonOutput(Builder builder) {
+        this.name = SmithyBuilder.requiredState("name", builder.name);
+        this.favoriteColor = builder.favoriteColor;
+        this.age = SmithyBuilder.requiredState("age", builder.age);
+        this.birthday = builder.birthday;
+        this.status = builder.status;
+        this.list = builder.list != null ? Collections.unmodifiableList(builder.list) : null;
+        this.set = builder.set != null ? Collections.unmodifiableSequencedSet(builder.set) : null;
+        this.requiredList = Collections.unmodifiableList(builder.requiredList);
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String favoriteColor() {
+        return favoriteColor;
+    }
+
+    public Integer age() {
+        return age;
+    }
+
+    public Instant birthday() {
+        return birthday;
+    }
+
+    public int status() {
+        return status;
+    }
+
+    public List<String> list() {
+        return list != null ? list : Collections.unmodifiableList(new ArrayList<>());
+    }
+
+    public boolean hasList() {
+        return list != null;
+    }
+
+    public SequencedSet<String> set() {
+        return set != null ? set : Collections.unmodifiableSequencedSet(new LinkedHashSet<>());
+    }
+
+    public boolean hasSet() {
+        return set != null;
+    }
+
+    public List<String> requiredList() {
+        return requiredList;
+    }
+
+    public boolean hasRequiredList() {
+        return requiredList != null;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringSerializer.serialize(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        PutPersonOutput that = (PutPersonOutput) other;
+        return Objects.equals(name, that.name)
+               && Objects.equals(favoriteColor, that.favoriteColor)
+               && Objects.equals(age, that.age)
+               && Objects.equals(birthday, that.birthday)
+               && Objects.equals(status, that.status)
+               && Objects.equals(list, that.list)
+               && Objects.equals(set, that.set)
+               && Objects.equals(requiredList, that.requiredList);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, favoriteColor, age, birthday, status, list, set, requiredList);
+    }
+
+    @Override
+    public void serialize(ShapeSerializer serializer) {
+        // Placeholder. Do nothing
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for {@link PutPersonOutput}.
+     */
+    public static final class Builder implements SdkShapeBuilder<PutPersonOutput> {
+        private String name;
+        private String favoriteColor;
+        private Integer age = 1;
+        private Instant birthday;
+        private Integer status;
+        private List<String> list;
+        private SequencedSet<String> set;
+        private List<String> requiredList;
+
+        private Builder() {}
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder favoriteColor(String favoriteColor) {
+            this.favoriteColor = favoriteColor;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder birthday(Instant birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder list(Collection<String> list) {
+            this.list = list != null ? new ArrayList<>(list) : null;
+            return this;
+        }
+
+        public Builder addAllList(Collection<String> list) {
+            if (this.list == null) {
+                this.list = new ArrayList<>(list);
+            } else {
+                this.list.addAll(list);
+            }
+            return this;
+        }
+
+        public Builder list(String list) {
+            if (this.list == null) {
+                this.list = new ArrayList<>();
+            }
+            this.list.add(list);
+            return this;
+        }
+
+        public Builder list(String... list) {
+            if (this.list == null) {
+                this.list = new ArrayList<>();
+            }
+            Collections.addAll(this.list, list);
+            return this;
+        }
+
+        public Builder set(Collection<String> set) {
+            this.set = set != null ? new LinkedHashSet<>(set) : null;
+            return this;
+        }
+
+        public Builder addAllSet(Collection<String> set) {
+            if (this.set == null) {
+                this.set = new LinkedHashSet<>(set);
+            } else {
+                this.set.addAll(set);
+            }
+            return this;
+        }
+
+        public Builder set(String set) {
+            if (this.set == null) {
+                this.set = new LinkedHashSet<>();
+            }
+            this.set.add(set);
+            return this;
+        }
+
+        public Builder set(String... set) {
+            if (this.set == null) {
+                this.set = new LinkedHashSet<>();
+            }
+            Collections.addAll(this.set, set);
+            return this;
+        }
+
+        public Builder requiredList(Collection<String> requiredList) {
+            this.requiredList = requiredList != null ? new ArrayList<>(requiredList) : null;
+            return this;
+        }
+
+        public Builder addAllRequiredList(Collection<String> requiredList) {
+            if (this.requiredList == null) {
+                this.requiredList = new ArrayList<>(requiredList);
+            } else {
+                this.requiredList.addAll(requiredList);
+            }
+            return this;
+        }
+
+        public Builder requiredList(String requiredList) {
+            if (this.requiredList == null) {
+                this.requiredList = new ArrayList<>();
+            }
+            this.requiredList.add(requiredList);
+            return this;
+        }
+
+        public Builder requiredList(String... requiredList) {
+            if (this.requiredList == null) {
+                this.requiredList = new ArrayList<>();
+            }
+            Collections.addAll(this.requiredList, requiredList);
+            return this;
+        }
+
+        @Override
+        public PutPersonOutput build() {
+            return new PutPersonOutput(this);
+        }
+
+        @Override
+        public Builder deserialize(ShapeDeserializer decoder) {
+            // PLACEHOLDER. Needs implementation
+            return this;
+        }
+    }
+}
+
