2df0a300f884b98172c4a82efe69f6241cb236d7
Hunter Mellema <hpm@amazon.com>
2024-04-23 14:26:55 -0600

    Add generated code for serialization impl

---
+++ examples/restjson-example/build/smithyprojections/restjson-example/source/java-codegen/software/amazon/smithy/java/runtime/example/codegen/model/PutPersonImageOutput.java
@@ -0,0 +1,80 @@
+/*
+ * Example file license header.
+ * File header line two
+ */
+
+
+package software.amazon.smithy.java.runtime.example.codegen.model;
+
+import java.util.Objects;
+import software.amazon.smithy.java.runtime.core.schema.SdkSchema;
+import software.amazon.smithy.java.runtime.core.schema.SdkShapeBuilder;
+import software.amazon.smithy.java.runtime.core.schema.SerializableShape;
+import software.amazon.smithy.java.runtime.core.serde.ShapeDeserializer;
+import software.amazon.smithy.java.runtime.core.serde.ShapeSerializer;
+import software.amazon.smithy.java.runtime.core.serde.ToStringSerializer;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeType;
+import software.amazon.smithy.utils.SmithyGenerated;
+
+@SmithyGenerated
+public final class PutPersonImageOutput implements SerializableShape {
+    public static final ShapeId ID = ShapeId.from("smithy.example#PutPersonImageOutput");
+
+    static final SdkSchema SCHEMA = SdkSchema.builder()
+        .id(ID)
+        .type(ShapeType.STRUCTURE)
+        .build();
+
+    private PutPersonImageOutput(Builder builder) {
+    }
+
+    @Override
+    public String toString() {
+        return ToStringSerializer.serialize(this);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        return other != null && getClass() == other.getClass();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash();
+    }
+
+    @Override
+    public void serialize(ShapeSerializer serializer) {
+        serializer.writeStruct(SCHEMA, st -> {
+
+        });
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for {@link PutPersonImageOutput}.
+     */
+    public static final class Builder implements SdkShapeBuilder<PutPersonImageOutput> {
+
+        private Builder() {}
+
+        @Override
+        public PutPersonImageOutput build() {
+            return new PutPersonImageOutput(this);
+        }
+
+        @Override
+        public Builder deserialize(ShapeDeserializer decoder) {
+            // PLACEHOLDER. Needs implementation
+            return this;
+        }
+    }
+}
+
