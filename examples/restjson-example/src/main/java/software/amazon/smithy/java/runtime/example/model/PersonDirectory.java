08ca8e807d5c297986579a32441761c2f23b186b
Jaykumar Gosar <gosar@amazon.com>
2024-05-17 15:12:52 -0700

    Separate async method to separate interface

--- examples/restjson-example/src/main/java/software/amazon/smithy/java/runtime/example/model/PersonDirectory.java
+++ examples/restjson-example/src/main/java/software/amazon/smithy/java/runtime/example/model/PersonDirectory.java
@@ -5,7 +5,6 @@
 
 package software.amazon.smithy.java.runtime.example.model;
 
-import java.util.concurrent.CompletableFuture;
 import software.amazon.smithy.java.runtime.core.Context;
 
 // An example of a generated service interface.
@@ -16,41 +15,17 @@ public interface PersonDirectory {
         return putPerson(input, Context.create());
     }
 
-    default PutPersonOutput putPerson(PutPersonInput input, Context context) {
-        return putPersonAsync(input, context).join();
-    }
-
-    default CompletableFuture<PutPersonOutput> putPersonAsync(PutPersonInput input) {
-        return putPersonAsync(input, Context.create());
-    }
-
-    CompletableFuture<PutPersonOutput> putPersonAsync(PutPersonInput input, Context context);
+    PutPersonOutput putPerson(PutPersonInput input, Context context);
 
     default PutPersonImageOutput putPersonImage(PutPersonImageInput input) {
         return putPersonImage(input, Context.create());
     }
 
-    default PutPersonImageOutput putPersonImage(PutPersonImageInput input, Context context) {
-        return putPersonImageAsync(input, context).join();
-    }
-
-    default CompletableFuture<PutPersonImageOutput> putPersonImageAsync(PutPersonImageInput input) {
-        return putPersonImageAsync(input, Context.create());
-    }
-
-    CompletableFuture<PutPersonImageOutput> putPersonImageAsync(PutPersonImageInput input, Context context);
+    PutPersonImageOutput putPersonImage(PutPersonImageInput input, Context context);
 
     default GetPersonImageOutput getPersonImage(GetPersonImageInput input) {
         return getPersonImage(input, Context.create());
     }
 
-    default GetPersonImageOutput getPersonImage(GetPersonImageInput input, Context context) {
-        return getPersonImageAsync(input, context).join();
-    }
-
-    default CompletableFuture<GetPersonImageOutput> getPersonImageAsync(GetPersonImageInput input) {
-        return getPersonImageAsync(input, Context.create());
-    }
-
-    CompletableFuture<GetPersonImageOutput> getPersonImageAsync(GetPersonImageInput input, Context context);
+    GetPersonImageOutput getPersonImage(GetPersonImageInput input, Context context);
 }
