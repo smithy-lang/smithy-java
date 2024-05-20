2d56e7897024252298f313229e75ecc9f5e83380
Hunter Mellema <hpm@amazon.com>
2024-05-20 15:50:14 -0600

    use 'values' instead of 'items'

--- codegen/core/src/it/java/software/amazon/smithy/java/codegen/SerdeTest.java
+++ codegen/core/src/it/java/software/amazon/smithy/java/codegen/SerdeTest.java
@@ -156,7 +156,7 @@ public class SerdeTest {
             );
 
             var exc = assertThrows(SdkSerdeException.class, () -> SetsInput.builder().deserialize(de));
-            assertTrue(exc.getMessage().contains("Member must have unique items"));
+            assertTrue(exc.getMessage().contains("Member must have unique values"));
         }
     }
 
