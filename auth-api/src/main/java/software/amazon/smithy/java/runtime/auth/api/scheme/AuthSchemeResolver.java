11865e9654016ebdc9dd87638cdc6c889c1adb9a
Hunter Mellema <hpm@amazon.com>
2024-08-19 08:04:55 -0600

    Updates the client pipeline auth flow to allow for generic auth Resolvers.

--- auth-api/src/main/java/software/amazon/smithy/java/runtime/auth/api/scheme/AuthSchemeResolver.java
+++ auth-api/src/main/java/software/amazon/smithy/java/runtime/auth/api/scheme/AuthSchemeResolver.java
@@ -5,13 +5,20 @@
 
 package software.amazon.smithy.java.runtime.auth.api.scheme;
 
+import java.util.ArrayList;
 import java.util.List;
+import software.amazon.smithy.java.runtime.auth.api.AuthProperties;
 
 /**
  * Resolves the authentication scheme that should be used to sign a request.
  */
 @FunctionalInterface
 public interface AuthSchemeResolver {
+    AuthSchemeResolver DEFAULT = new DefaultAuthSchemeResolver();
+    AuthSchemeResolver NO_AUTH = (param) -> List.of(
+        new AuthSchemeOption(NoAuthAuthScheme.INSTANCE.schemeId(), AuthProperties.empty(), AuthProperties.empty())
+    );
+
     /**
      * Resolve the auth scheme options using the given parameters.
      *
@@ -22,4 +29,16 @@ public interface AuthSchemeResolver {
      * @return the resolved auth scheme options.
      */
     List<AuthSchemeOption> resolveAuthScheme(AuthSchemeResolverParams params);
+
+    final class DefaultAuthSchemeResolver implements AuthSchemeResolver {
+
+        @Override
+        public List<AuthSchemeOption> resolveAuthScheme(AuthSchemeResolverParams params) {
+            var result = new ArrayList<AuthSchemeOption>();
+            for (var schemeId : params.operationAuthSchemes()) {
+                result.add(new AuthSchemeOption(schemeId, AuthProperties.empty(), AuthProperties.empty()));
+            }
+            return result;
+        }
+    }
 }
