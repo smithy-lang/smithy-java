b3e306a49ee092b065f29fef30f066e3bf33d1f2
Hunter Mellema <hpm@amazon.com>
2024-08-28 15:01:01 -0600

    Add default resolvers

--- aws/client-http/src/main/java/software/amazon/smithy/java/aws/runtime/client/http/AwsBasePlugin.java
+++ aws/client-http/src/main/java/software/amazon/smithy/java/aws/runtime/client/http/AwsBasePlugin.java
@@ -5,9 +5,13 @@
 
 package software.amazon.smithy.java.runtime.aws.http;
 
+import java.util.List;
 import java.util.Objects;
 import software.amazon.smithy.java.logging.InternalLogger;
-import software.amazon.smithy.java.runtime.aws.http.auth.identity.AwsIdentityResolver;
+import software.amazon.smithy.java.runtime.auth.api.identity.IdentityResolver;
+import software.amazon.smithy.java.runtime.aws.http.auth.identity.AwsCredentialsIdentity;
+import software.amazon.smithy.java.runtime.aws.http.auth.identity.EnvironmentVariableIdentityResolver;
+import software.amazon.smithy.java.runtime.aws.http.auth.identity.SystemPropertiesIdentityResolver;
 import software.amazon.smithy.java.runtime.client.core.ClientConfig;
 import software.amazon.smithy.java.runtime.client.core.ClientPlugin;
 import software.amazon.smithy.java.runtime.client.core.annotations.Configuration;
@@ -17,7 +21,11 @@ import software.amazon.smithy.java.runtime.client.core.annotations.Configuration
  */
 public final class AwsBasePlugin implements ClientPlugin {
     private static final InternalLogger LOGGER = InternalLogger.getLogger(AwsBasePlugin.class);
-
+    // TODO: Add remaining default identity resolvers in chain
+    // see: https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/credentials-chain.html
+    private static final IdentityResolver<AwsCredentialsIdentity> DEFAULT_RESOLVER_CHAIN = IdentityResolver.chain(
+        List.of(new SystemPropertiesIdentityResolver(), new EnvironmentVariableIdentityResolver())
+    );
     private String region;
 
     @Configuration
@@ -32,7 +40,6 @@ public final class AwsBasePlugin implements ClientPlugin {
             AwsConfigurationProperties.REGION,
             Objects.requireNonNull(region, "Region property must be set")
         );
-        // TODO: Replace with a default identity resolver chain.
-        config.addIdentityResolver(new AwsIdentityResolver());
+        config.addIdentityResolver(DEFAULT_RESOLVER_CHAIN);
     }
 }
--- aws/client-http/src/main/java/software/amazon/smithy/java/aws/runtime/client/http/auth/identity/AwsCredentialsResolver.java
+++ aws/client-http/src/main/java/software/amazon/smithy/java/aws/runtime/client/http/auth/identity/AwsCredentialsResolver.java
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+<<<<<<< HEAD:aws/client-http/src/main/java/software/amazon/smithy/java/aws/runtime/client/http/auth/identity/AwsCredentialsResolver.java
 package software.amazon.smithy.java.aws.runtime.client.http.auth.identity;
 
 import software.amazon.smithy.java.runtime.auth.api.identity.IdentityResolver;
@@ -10,6 +11,12 @@ import software.amazon.smithy.java.runtime.auth.api.identity.IdentityResolver;
 /**
  * An {@link IdentityResolver} that resolves a {@link AwsCredentialsIdentity} for authentication.
  */
+=======
+package software.amazon.smithy.java.runtime.aws.http.auth.identity;
+
+import software.amazon.smithy.java.runtime.auth.api.identity.IdentityResolver;
+
+>>>>>>> 95767d49 (Add default resolvers):aws-client-http/src/main/java/software/amazon/smithy/java/runtime/aws/http/auth/identity/AwsCredentialsResolver.java
 interface AwsCredentialsResolver extends IdentityResolver<AwsCredentialsIdentity> {
     @Override
     default Class<AwsCredentialsIdentity> identityType() {
