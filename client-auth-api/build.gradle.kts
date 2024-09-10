b4c1ae8920e10fc510010ecb3b815b2cece32def
Jaykumar Gosar <gosar@amazon.com>
2024-09-10 11:48:35 -0700

    Move client focused AuthScheme to client-auth-api
    With this, auth-api contains more generic interfaces for Identity and Signer
    which can be used in other use cases outside of a client. And auth-api has
    minimal dependencies - removes dependency on smithy-model.

---
+++ client-auth-api/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("smithy-java.module-conventions")
+}
+
+description = "This module provides endpoint resolver API"
+
+extra["displayName"] = "Smithy :: Java :: Endpoint Resolver API"
+extra["moduleName"] = "software.amazon.smithy.smithy.java.client-endpoint-api"
+
+dependencies {
+    api(project(":context"))
+    api(project(":auth-api"))
+    api(libs.smithy.model)
+}
