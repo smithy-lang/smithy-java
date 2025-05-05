c505e03ca66106b2e06aa67eccbd82bbcde7f9ac
Michael Dowling <michael@mtdowling.com>
2025-05-05 11:41:39 -0500

    Add tests for stdlib

--- client/client-endpoint-rules/src/main/java/software/amazon/smithy/java/client/endpointrules/Stdlib.java
+++ client/client-endpoint-rules/src/main/java/software/amazon/smithy/java/client/endpointrules/Stdlib.java
@@ -7,12 +7,13 @@ package software.amazon.smithy.java.client.endpointrules;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import software.amazon.smithy.java.context.Context;
+import software.amazon.smithy.java.io.uri.URLEncoding;
 
 /**
- * Implements stdlib functions of the rules engine that weren't promoted to opcodes.
+ * Implements stdlib functions of the rules engine that weren't promoted to opcodes (GetAttr, isset, not).
  */
 enum Stdlib implements VmFunction {
     // https://smithy.io/2.0/additional-specs/rules-engine/standard-library.html#stringequals-function
@@ -35,33 +36,52 @@ enum Stdlib implements VmFunction {
     SUBSTRING("substring", 4) {
         @Override
         public Object apply(Object... operands) {
-            String input = (String) operands[0];
-            int start = (int) operands[1];
-            int end = (int) operands[2];
+            // software.amazon.smithy.rulesengine.language.syntax.expressions.functions.Substring.Definition.evaluate
+            String str = (String) operands[0];
+            int startIndex = (int) operands[1];
+            int stopIndex = (int) operands[2];
             boolean reverse = (boolean) operands[3];
 
-            if (input == null || input.length() < (end - start)) {
+            for (int i = 0; i < str.length(); i++) {
+                if (!(str.charAt(i) <= 127)) {
+                    return null;
+                }
+            }
+
+            if (startIndex >= stopIndex || str.length() < stopIndex) {
                 return null;
             }
 
-            var result = input.substring(start, end);
-
-            // TODO: Did we really add a reverse functionality to this rule?
-            if (reverse) {
-                result = new StringBuilder(result).reverse().toString();
+            if (!reverse) {
+                return str.substring(startIndex, stopIndex);
+            } else {
+                int revStart = str.length() - stopIndex;
+                int revStop = str.length() - startIndex;
+                return str.substring(revStart, revStop);
             }
-
-            return result;
         }
     },
 
     // https://smithy.io/2.0/additional-specs/rules-engine/standard-library.html#isvalidhostlabel-function
     IS_VALID_HOST_LABEL("isValidHostLabel", 1) {
+        private static final Pattern HOST_LABEL_PATTERN =
+                Pattern.compile("^[a-zA-Z\\d][a-zA-Z\\d\\-]{0,62}$");
+
         @Override
-        public Object apply1(Object arg) {
-            // TODO: Implement this.
-            //var input = (String) operands[0];
-            throw new UnsupportedOperationException("isValidHostLabel is not yet implemented");
+        public Object apply2(Object arg1, Object arg2) {
+            var hostLabel = (String) arg1;
+            var allowDots = (Boolean) arg2;
+            if (allowDots == null || !allowDots) {
+                return HOST_LABEL_PATTERN.matcher(hostLabel).matches();
+            } else {
+                // ensure that empty matches at the end are included
+                for (String subLabel : hostLabel.split("[.]", -1)) {
+                    if (!HOST_LABEL_PATTERN.matcher(subLabel).matches()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     },
 
@@ -82,34 +102,7 @@ enum Stdlib implements VmFunction {
     URI_ENCODE("uriEncode", 1) {
         @Override
         public Object apply1(Object arg) {
-            String input = (String) arg;
-            StringBuilder encoded = new StringBuilder();
-            for (int i = 0; i < input.length();) {
-                int codePoint = input.codePointAt(i);
-                i += Character.charCount(codePoint);
-                if (isUnreserved(codePoint)) {
-                    encoded.appendCodePoint(codePoint);
-                } else {
-                    byte[] bytes = new String(Character.toChars(codePoint)).getBytes(StandardCharsets.UTF_8);
-                    for (byte b : bytes) {
-                        encoded.append('%');
-                        encoded.append(String.format("%02X", b));
-                    }
-                }
-            }
-            return encoded.toString();
-        }
-
-        private static boolean isUnreserved(int codePoint) {
-            return (codePoint >= 'A' && codePoint <= 'Z') ||
-                    (codePoint >= 'a' && codePoint <= 'z')
-                    ||
-                    (codePoint >= '0' && codePoint <= '9')
-                    ||
-                    codePoint == '-'
-                    || codePoint == '_'
-                    || codePoint == '.'
-                    || codePoint == '~';
+            return URLEncoding.encodeUnreserved((String) arg, false);
         }
     };
 
