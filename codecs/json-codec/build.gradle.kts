1cd8fe90dcae9a46d06d7f820294713528a1ae88
Adwait Kumar Singh <adwsingh@amazon.com>
2025-05-14 09:30:41 -0700

    Shade Jackson

--- codecs/json-codec/build.gradle.kts
+++ codecs/json-codec/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("smithy-java.module-conventions")
+    alias(libs.plugins.shadow)
 }
 
 description = "This module provides json functionality"
@@ -10,4 +11,44 @@ extra["moduleName"] = "software.amazon.smithy.java.json"
 dependencies {
     api(project(":core"))
     implementation(libs.jackson.core)
+}
+
+tasks.shadowJar {
+    dependencies {
+        include(
+            dependency(
+                libs.jackson.core
+                    .get()
+                    .toString(),
+            ),
+        )
+        relocate("com.fasterxml.jackson.core", "software.amazon.smithy.java.internal.com.fasterxml.jackson.core")
+    }
+    archiveClassifier.set("")
+    mergeServiceFiles()
+}
+
+(components["shadow"] as AdhocComponentWithVariants).addVariantsFromConfiguration(configurations.apiElements.get()) {
+}
+
+configurePublishing {
+    customComponent = components["shadow"] as SoftwareComponent
+}
+
+tasks.jar {
+    enabled = true
+    dependsOn(tasks.shadowJar)
+    outputs.files(
+        tasks.shadowJar
+            .get()
+            .outputs.files,
+    )
+}
+
+artifacts {
+    configurations.archives
+        .get()
+        .artifacts
+        .clear()
+    archives(tasks.shadowJar.get())
 }
