bc0da51b1650254c17f151d4b293473c02bb6115
Dean Shaw <shwdean@amazon.com>
2025-06-09 17:41:18 -0400

    Address comments

--- aws/aws-service-bundler/src/main/java/software/amazon/smithy/java/aws/servicebundle/bundler/ApiStandardTerminology.java
+++ aws/aws-service-bundler/src/main/java/software/amazon/smithy/java/aws/servicebundle/bundler/ApiStandardTerminology.java
@@ -40,12 +40,15 @@ public final class ApiStandardTerminology {
             "Untag",
             "Update");
 
+    private  static final Set<String> READ_ONLY_API_PREFIXES = withCommonsPrefixes(READ_ONLY_TERMS);
+    private  static final Set<String> WRITE_API_PREFIXES = withCommonsPrefixes(WRITE_TERMS);
+
     public static Set<String> getReadOnlyApiPrefixes() {
-        return withCommonsPrefixes(READ_ONLY_TERMS);
+        return READ_ONLY_API_PREFIXES;
     }
 
     public static Set<String> getWriteApiPrefixes() {
-        return withCommonsPrefixes(WRITE_TERMS);
+        return WRITE_API_PREFIXES;
     }
 
     private ApiStandardTerminology() {}
