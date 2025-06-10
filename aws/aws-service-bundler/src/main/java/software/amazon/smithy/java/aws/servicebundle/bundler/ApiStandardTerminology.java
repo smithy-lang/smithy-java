3f36247e2fd83edc7719ce6cba64fd0f3d54a5ed
Dean Shaw <shwdean@amazon.com>
2025-06-10 17:15:06 -0400

    Fix spotbugs and remove public visibility

--- aws/aws-service-bundler/src/main/java/software/amazon/smithy/java/aws/servicebundle/bundler/ApiStandardTerminology.java
+++ aws/aws-service-bundler/src/main/java/software/amazon/smithy/java/aws/servicebundle/bundler/ApiStandardTerminology.java
@@ -5,12 +5,14 @@
 
 package software.amazon.smithy.java.aws.servicebundle.bundler;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class ApiStandardTerminology {
+final class ApiStandardTerminology {
 
     private static final Set<String> COMMON_PREFIXES = Set.of("Batch");
     private static final Set<String> READ_ONLY_TERMS =
@@ -43,10 +45,12 @@ public final class ApiStandardTerminology {
     private static final Set<String> READ_ONLY_API_PREFIXES = withCommonsPrefixes(READ_ONLY_TERMS);
     private static final Set<String> WRITE_API_PREFIXES = withCommonsPrefixes(WRITE_TERMS);
 
+    @SuppressFBWarnings(value = "MS", justification = "Internal representation already immutable")
     public static Set<String> getReadOnlyApiPrefixes() {
         return READ_ONLY_API_PREFIXES;
     }
 
+    @SuppressFBWarnings(value = "MS", justification = "Internal representation already immutable")
     public static Set<String> getWriteApiPrefixes() {
         return WRITE_API_PREFIXES;
     }
