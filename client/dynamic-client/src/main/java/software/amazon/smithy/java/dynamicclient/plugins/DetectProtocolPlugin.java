0aff7bab900131707a2bad5e6470d858647a6ce5
Michael Dowling <michael@mtdowling.com>
2025-11-14 11:59:53 -0600

    Pick only protocols that match the transport

--- client/dynamic-client/src/main/java/software/amazon/smithy/java/dynamicclient/plugins/DetectProtocolPlugin.java
+++ client/dynamic-client/src/main/java/software/amazon/smithy/java/dynamicclient/plugins/DetectProtocolPlugin.java
@@ -6,32 +6,36 @@
 package software.amazon.smithy.java.dynamicclient.plugins;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.ServiceLoader;
 import software.amazon.smithy.java.client.core.ClientConfig;
 import software.amazon.smithy.java.client.core.ClientPlugin;
+import software.amazon.smithy.java.client.core.ClientProtocol;
 import software.amazon.smithy.java.client.core.ClientProtocolFactory;
+import software.amazon.smithy.java.client.core.ClientTransport;
 import software.amazon.smithy.java.client.core.ProtocolSettings;
 import software.amazon.smithy.java.dynamicclient.settings.ModelSetting;
 import software.amazon.smithy.java.dynamicclient.settings.ServiceIdSetting;
 import software.amazon.smithy.model.knowledge.ServiceIndex;
+import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.model.traits.Trait;
 
 /**
  * DynamicClient attempts to detect the protocol to use from the model.
+ *
+ * <p>If a transport is already manually defined, then this plugin only picks a protocol that matches the transport
+ * based on the presence of a protocol trait and a {@link ClientProtocolFactory}.
  */
 public final class DetectProtocolPlugin implements ClientPlugin {
 
     public static final DetectProtocolPlugin INSTANCE = new DetectProtocolPlugin();
-    private static final List<ClientProtocolFactory<Trait>> PROTOCOL_FACTORIES;
+    private static final List<ClientProtocolFactory<Trait>> PROTOCOL_FACTORIES = new ArrayList<>();
 
     static {
-        List<ClientProtocolFactory<Trait>> factories = new ArrayList<>();
         for (var protocolImpl : ServiceLoader.load(ClientProtocolFactory.class)) {
-            factories.add(protocolImpl);
+            PROTOCOL_FACTORIES.add(protocolImpl);
         }
-        PROTOCOL_FACTORIES = Collections.unmodifiableList(factories);
     }
 
     @Override
@@ -39,7 +43,8 @@ public final class DetectProtocolPlugin implements ClientPlugin {
         return Phase.FIRST;
     }
 
-    // TODO: pick a protocol that matches the transport if transport is set.
+    // TODO: we currently just pick the first found protocol. We could add something like SMITHY_PREFERRED_PROTOCOLS
+    //  later to influence the picked protocol.
     @Override
     public void configureClient(ClientConfig.Builder config) {
         if (config.protocol() != null) {
@@ -65,17 +70,55 @@ public final class DetectProtocolPlugin implements ClientPlugin {
                             + "were found on service " + service);
         }
 
-        for (var protocolImpl : PROTOCOL_FACTORIES) {
-            if (protocols.containsKey(protocolImpl.id())) {
-                var settings = ProtocolSettings.builder().service(service).build();
-                config.protocol(protocolImpl.createProtocol(settings, protocols.get(protocolImpl.id())));
-                return;
-            }
+        var transport = config.transport();
+        ClientProtocol<?, ?> protocol = transport != null
+                ? findProtocolMatchingTransport(service, protocols, transport)
+                : findProtocol(service, protocols);
+
+        if (protocol != null) {
+            config.protocol(protocol);
+            return;
         }
 
         throw new IllegalArgumentException(
                 "Could not find any matching protocol implementations for the "
                         + "following protocol traits attached to service " + service
-                        + ": " + protocols.keySet());
+                        + ": " + protocols.keySet()
+                        + " (transport set?=" + (transport == null ? "no" : transport.getClass().getName()) + ")");
+    }
+
+    private static ClientProtocol<?, ?> findProtocol(ShapeId service, Map<ShapeId, Trait> protocols) {
+        for (var protocolImpl : PROTOCOL_FACTORIES) {
+            if (protocols.containsKey(protocolImpl.id())) {
+                return createProtocol(service, protocols, protocolImpl);
+            }
+        }
+        return null;
+    }
+
+    private static ClientProtocol<?, ?> createProtocol(
+            ShapeId service,
+            Map<ShapeId, Trait> protocols,
+            ClientProtocolFactory<Trait> protocolImpl
+    ) {
+        var settings = ProtocolSettings.builder().service(service).build();
+        return protocolImpl.createProtocol(settings, protocols.get(protocolImpl.id()));
+    }
+
+    private static ClientProtocol<?, ?> findProtocolMatchingTransport(
+            ShapeId service,
+            Map<ShapeId, Trait> protocols,
+            ClientTransport<?, ?> transport
+    ) {
+        for (var protocolImpl : PROTOCOL_FACTORIES) {
+            if (protocols.containsKey(protocolImpl.id())) {
+                var protocol = createProtocol(service, protocols, protocolImpl);
+                // Only return the protocol if it has the same message exchange as the transport.
+                if (protocol.messageExchange().equals(transport.messageExchange())) {
+                    return protocol;
+                }
+            }
+        }
+        return null;
     }
 }
