77d3588daa8f0d4786518e3bc0a2a4459154f4ec
Joe Wu <yizhouwu@amazon.com>
2025-11-14 09:47:53 -0800

    Add document discrinator sanitizer for JSON protocols

--- aws/client/aws-client-awsjson/src/main/java/software/amazon/smithy/java/aws/client/awsjson/AwsJson11Protocol.java
+++ aws/client/aws-client-awsjson/src/main/java/software/amazon/smithy/java/aws/client/awsjson/AwsJson11Protocol.java
@@ -10,6 +10,7 @@ import software.amazon.smithy.aws.traits.protocols.AwsJson1_1Trait;
 import software.amazon.smithy.java.client.core.ClientProtocol;
 import software.amazon.smithy.java.client.core.ClientProtocolFactory;
 import software.amazon.smithy.java.client.core.ProtocolSettings;
+import software.amazon.smithy.java.json.ErrorTypeSanitizer;
 import software.amazon.smithy.model.shapes.ShapeId;
 
 /**
@@ -24,7 +25,7 @@ public final class AwsJson11Protocol extends AwsJsonProtocol {
      *                discriminator of documents that use relative shape IDs.
      */
     public AwsJson11Protocol(ShapeId service) {
-        super(TRAIT_ID, service);
+        super(TRAIT_ID, service, ErrorTypeSanitizer::REMOVE_NAMESPACE_AND_URI);
     }
 
     @Override
