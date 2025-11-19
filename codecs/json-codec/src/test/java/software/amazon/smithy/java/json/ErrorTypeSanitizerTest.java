6539ddadee820ea836f593e9029eafbee1895ef0
Joe Wu <yizhouwu@amazon.com>
2025-11-19 13:50:30 -0800

    Apply spotless

--- codecs/json-codec/src/test/java/software/amazon/smithy/java/json/ErrorTypeSanitizerTest.java
+++ codecs/json-codec/src/test/java/software/amazon/smithy/java/json/ErrorTypeSanitizerTest.java
@@ -1,16 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package software.amazon.smithy.java.json;
 
-import java.util.List;
-import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class ErrorTypeSanitizerTest {
     private static final String SIMPLE_TYPE = "FooError";
     private static final String TYPE_WITH_URI = "FooError:http://amazon.com/smithy/com.amazon.smithy.validate/";
     private static final String TYPE_WITH_NAMESPACE = "aws.protocoltests.restjson#FooError";
-    private static final String TYPE_WITH_NAMESPACE_AND_URI = "aws.protocoltests.restjson#FooError:http://amazon.com/smithy/com.amazon.smithy.validate/";
+    private static final String TYPE_WITH_NAMESPACE_AND_URI =
+            "aws.protocoltests.restjson#FooError:http://amazon.com/smithy/com.amazon.smithy.validate/";
     @Test
     public void testRemoveUri() {
         String expected = "FooError";
