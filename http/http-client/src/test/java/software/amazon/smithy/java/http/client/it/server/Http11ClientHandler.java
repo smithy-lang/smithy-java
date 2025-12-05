a2a7f10e4e403ec7729ad3ed057ca1b5d25c54a2
Manuel Sugawara (∩｀-´)⊃━炎炎炎炎炎 <sugmanue@amazon.com>
2025-12-04 17:25:59 -0800

    Add a Netty server for local testing

---
+++ http/http-client/src/test/java/software/amazon/smithy/java/http/client/it/server/Http11ClientHandler.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.java.http.client.it.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.LastHttpContent;
+
+public interface Http11ClientHandler {
+
+    void onFullRequest(ChannelHandlerContext ctx, FullHttpRequest request);
+
+    void onRequest(ChannelHandlerContext ctx, HttpRequest request);
+
+    void onContent(ChannelHandlerContext ctx, HttpContent content);
+
+    void onLastContent(ChannelHandlerContext ctx, LastHttpContent content);
+
+    void onException(ChannelHandlerContext ctx, Throwable cause);
+}
