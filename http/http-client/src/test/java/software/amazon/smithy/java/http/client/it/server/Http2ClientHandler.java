26611913c20a4e680f136e833982a010ce68657f
Manuel Sugawara (∩｀-´)⊃━炎炎炎炎炎 <sugmanue@amazon.com>
2025-12-04 17:25:59 -0800

    Add a Netty server for local testing

---
+++ http/http-client/src/test/java/software/amazon/smithy/java/http/client/it/server/Http2ClientHandler.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.java.http.client.it.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http2.Http2DataFrame;
+import io.netty.handler.codec.http2.Http2HeadersFrame;
+
+public interface Http2ClientHandler {
+
+    void onHeadersFrame(ChannelHandlerContext ctx, Http2HeadersFrame frame);
+
+    void onDataFrame(ChannelHandlerContext ctx, Http2DataFrame frame);
+
+    void onException(ChannelHandlerContext ctx, Throwable cause);
+
+}
