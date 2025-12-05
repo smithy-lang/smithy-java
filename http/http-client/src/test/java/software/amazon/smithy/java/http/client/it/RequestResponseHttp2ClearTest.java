ed973b2e734fc7092b1f867d39080bbf4db472f8
Manuel Sugawara (∩｀-´)⊃━炎炎炎炎炎 <sugmanue@amazon.com>
2025-12-04 17:25:59 -0800

    Add a Netty server for local testing

---
+++ http/http-client/src/test/java/software/amazon/smithy/java/http/client/it/RequestResponseHttp2ClearTest.java
@@ -0,0 +1,83 @@
+package software.amazon.smithy.java.http.client.it;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.java.http.client.HttpClient;
+import software.amazon.smithy.java.http.client.connection.HttpConnectionPool;
+import software.amazon.smithy.java.http.client.connection.HttpVersionPolicy;
+import software.amazon.smithy.java.http.client.dns.DnsResolver;
+import software.amazon.smithy.java.http.client.it.server.MultiplexingHttp2ClientHandler;
+import software.amazon.smithy.java.http.client.it.server.NettyTestServer;
+import software.amazon.smithy.java.http.client.it.server.RequestCapturingHttp2ClientHandler;
+import software.amazon.smithy.java.http.client.it.server.TextResponseHttp2ClientHandler;
+import software.amazon.smithy.java.http.api.HttpVersion;
+
+public class RequestResponseHttp2ClearTest {
+    private static final String RESPONSE_CONTENTS = "Response sent from Http2ClearTest";
+    private static final String REQUEST_CONTENTS = "Request sent from Http2ClearTest";
+    private RequestCapturingHttp2ClientHandler requestCapturingHandler;
+    private NettyTestServer server;
+    private HttpClient client;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        requestCapturingHandler = new RequestCapturingHttp2ClientHandler();
+        var multiplexer = new MultiplexingHttp2ClientHandler(requestCapturingHandler,
+                new TextResponseHttp2ClientHandler(RESPONSE_CONTENTS));
+        server = NettyTestServer.builder()
+                .httpVersion(HttpVersion.HTTP_2)
+                .h2ConnectionMode(NettyTestServer.H2ConnectionMode.PRIOR_KNOWLEDGE)
+                .http2HandlerFactory((ctx) -> multiplexer)
+                // fixed port to wireshark the traffic
+                .port(8080)
+                .build();
+        server.start();
+
+
+        DnsResolver staticDns = DnsResolver.staticMapping(Map.of(
+                "localhost",
+                List.of(InetAddress.getLoopbackAddress())));
+        client = HttpClient.builder()
+                .connectionPool(HttpConnectionPool.builder()
+                        .maxConnectionsPerRoute(10)
+                        .maxTotalConnections(10)
+                        .maxIdleTime(Duration.ofMinutes(1))
+                        .httpVersionPolicy(HttpVersionPolicy.H2C_PRIOR_KNOWLEDGE)
+                        .dnsResolver(staticDns)
+                        .build())
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        server.stop();
+    }
+
+    @Test
+    void canSendRequestAndReadResponse() throws Exception {
+        // -- Arrange
+        var request = TestUtils.plainTextHttp2Request("http://localhost:" + server.getPort(), REQUEST_CONTENTS);
+
+        // -- Act
+        var response = client.send(request);
+        var bodyByteBuf = response.body().asByteBuffer();
+        var bytes = new byte[bodyByteBuf.remaining()];
+        bodyByteBuf.get(bytes);
+        var responseBody = new String(bytes, StandardCharsets.UTF_8);
+        client.close();
+
+        // -- Assert
+        var capturedRequestBody = requestCapturingHandler.capturedBody().toString(StandardCharsets.UTF_8);
+        assertEquals(REQUEST_CONTENTS, capturedRequestBody);
+        assertEquals(RESPONSE_CONTENTS, responseBody);
+    }
+}
---
+++ http/http-client/src/test/java/software/amazon/smithy/java/http/client/it/server/Http2ClientHandlerFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.java.http.client.it.server;
+
+import io.netty.channel.ChannelHandlerContext;
+
+@FunctionalInterface
+public interface Http2ClientHandlerFactory {
+
+    Http2ClientHandler create(ChannelHandlerContext ctx);
+}
